8a9e9fe6e94296a3b3d5fcce81f69c87437a58ce
Peter Kral <peter.kral@pdfix.net>
2025-06-02 16:20:38 +0200

    Source code updated: - argparsing as in other external actions - subcommand structure as in other external actions - get config as in other external actions - added func docs

--- src/main.py
+++ src/main.py
@@ -1,43 +1,142 @@
 import argparse
 import os
-import shutil
 import subprocess
 import sys
+import traceback
 from pathlib import Path
+from typing import Optional
 
 
-def get_config(path: str) -> None:
-    if path is None:
-        with open(
-            os.path.join(Path(__file__).parent.absolute(), "../config.json"),
-            "r",
-            encoding="utf-8",
-        ) as f:
-            print(f.read())
-    else:
-        src = os.path.join(Path(__file__).parent.absolute(), "../config.json")
-        dst = path
-        shutil.copyfile(src, dst)
+def set_arguments(
+    parser: argparse.ArgumentParser,
+    names: list,
+    output_help: str = "",
+) -> None:
+    """
+    Set arguments for the parser based on the provided names and options.
+
+    Args:
+        parser (argparse.ArgumentParser): The argument parser to set arguments for.
+        names (list): List of argument names to set.
+        output_help (str): Help shown for output argument. Defaults to "".
+    """
+    for name in names:
+        match name:
+            case "format":
+                parser.add_argument(
+                    "--format",
+                    type=str,
+                    default="xml",
+                    choices=["raw", "xml", "html", "text", "json"],
+                    help="Format of output",
+                )
+            case "input":
+                parser.add_argument("--input", "-i", type=str, required=True, help="The input PDF file")
+            case "maxfailuresdisplayed":
+                parser.add_argument("--maxfailuresdisplayed", type=int, default=-1, help="Max failures displayed")
+            case "output":
+                parser.add_argument("--output", "-o", type=str, help=output_help)
 
 
-def run_arlington(command: list) -> tuple:
-    """Execute a shell command and capture its output and return code.
+def run_config_subcommand(args) -> None:
+    get_pdfix_config(args.output)
+
+
+def get_pdfix_config(path: str) -> None:
+    """
+    If Path is not provided, output content of config.
+    If Path is provided, copy config to destination path.
+
+    Args:
+        path (string): Destination path for config.json file
+    """
+    config_path = os.path.join(Path(__file__).parent.absolute(), "../config.json")
+
+    with open(config_path, "r", encoding="utf-8") as file:
+        if path is None:
+            print(file.read())
+        else:
+            with open(path, "w") as out:
+                out.write(file.read())
+
+
+def run_validation_subcommand(args) -> None:
+    input_file = args.input
+
+    if not os.path.isfile(input_file):
+        raise Exception(f"Error: The input file '{input_file}' does not exist.")
+
+    if not input_file.lower().endswith(".pdf"):
+        raise Exception("Input file must be PDF")
+
+    output_file: Optional[str] = args.output
+    maxfailuresdisplayed: int = args.maxfailuresdisplayed
+    format: str = args.format
+
+    run_validation(input_file, output_file, maxfailuresdisplayed, format)
+
+
+def run_validation(
+    input_file: str,
+    output_file: Optional[str],
+    maxfailuresdisplayed: int,
+    format: str,
+) -> None:
+    """
+    Runs validation using Arlington java program in subprocess.
+
+    Args:
+        input_file (str): Path to input PDF file.
+        output_file (Optional[str]): Either path to output file or None when output goes to standart output.
+        maxfailuresdisplayed (str): Max failures displayed
+        format (str): Format of output like json, xml, ...
+    """
+    try:
+        java_program_path = os.path.join(
+            Path(__file__).parent.absolute(), "../res/greenfield-apps-arlington-1.27.0-SNAPSHOT.jar"
+        )
+        command = [
+            "java",
+            "-jar",
+            java_program_path,
+            "--maxfailuresdisplayed",
+            str(maxfailuresdisplayed),
+            "--format",
+            format,
+        ]
+
+        command.append(input_file)
+
+        stdout, stderr = run_subprocess(command)
+
+        if output_file:
+            with open(output_file, "w+", encoding="utf-8") as out:
+                out.write(stdout)
+        else:
+            print(stdout)
+
+        if stderr:
+            print(stderr, file=sys.stderr)
+
+    except Exception as e:
+        raise Exception(f"Failed to run validation: {e}")
+
+
+def run_subprocess(command: list) -> tuple:
+    """
+    Execute a shell command and capture its output and return code.
 
     This function runs the validation as shell command using the `subprocess.Popen`
     method, captures the standard output (stdout), standard error (stderr),
     and the return code of the process.
 
     Args:
-    ----
         command (str): The shell command to execute.
 
     Returns:
-    -------
         tuple: A tuple containing:
             - stdout (str): The standard output of the command.
             - stderr (str): The standard error of the command.
-            - return_code (int): The return code of the command
-              (0 indicates success, non-zero indicates failure).
 
     """
     process = subprocess.Popen(
@@ -47,12 +146,9 @@ def run_arlington(command: list) -> tuple:
         shell=True,
         text=True,
     )
-
     stdout, stderr = process.communicate()
 
-    return_code = process.returncode
-
-    return stdout, stderr, return_code
+    return stdout, stderr
 
 
 def main():
@@ -62,100 +158,47 @@ def main():
 
     subparsers = parser.add_subparsers(dest="subparser")
 
-    # config subparser
-    pars_config = subparsers.add_parser(
+    # Config subcommand
+    config_subparser = subparsers.add_parser(
         "config",
         help="Extract config file for integration",
     )
-    pars_config.add_argument(
-        "-o",
-        "--output",
-        type=str,
-        help="Output to save the config JSON file. Application output\
-              is used if not provided",
+    set_arguments(
+        config_subparser,
+        ["output"],
+        False,
+        "Output to save the config JSON file. Application output is used if not provided.",
     )
+    config_subparser.set_defaults(func=run_config_subcommand)
 
-    pars_validate = subparsers.add_parser(
+    # Validate subcommand
+    validate_subparser = subparsers.add_parser(
         "validate",
-        help="Run alternate text description",
+        help="Run validation of PDF document",
     )
-
-    pars_validate.add_argument("-i", "--input", type=str, help="The input PDF file")
-    pars_validate.add_argument(
-        "-o",
-        "--output",
-        type=str,
-        help="The output validation file",
+    set_arguments(
+        validate_subparser,
+        ["input", "output", "maxfailuresdisplayed", "format"],
+        True,
+        "The output validation file",
     )
-
-    pars_validate.add_argument(
-        "--maxfailuresdisplayed",
-        type=int,
-        default=-1,
-        help="Max failures displayed",
-    )
-
-    pars_validate.add_argument(
-        "--format",
-        type=str,
-        default="xml",
-        choices=["raw", "xml", "html", "text", "json"],
-    )
+    validate_subparser.set_defaults(func=run_validation_subcommand)
 
     try:
         args = parser.parse_args()
     except SystemExit as e:
         if e.code == 0:  # This happens when --help is used, exit gracefully
             sys.exit(0)
-        print("Failed to parse arguments. Please check the usage and try again.")
+        print("Failed to parse arguments. Please check the usage and try again.", file=sys.stderr)
         sys.exit(1)
 
-    if args.subparser == "config":
-        get_config(args.output)
-        sys.exit(0)
-
-    elif args.subparser == "validate":
-        if not args.input:
-            pars_validate.error("The following arguments are required: -i/--input")
-
-        input_file = args.input
-        output_file = args.output
-
-        if not os.path.isfile(input_file):
-            sys.exit(f"Error: The input file '{input_file}' does not exist.")
-            return
-
-        if input_file.lower().endswith(".pdf"):
-            try:
-                command = [
-                    "java",
-                    "-jar",
-                    "/usr/validation/res/greenfield-apps-arlington-1.27.0-SNAPSHOT.jar",
-                    "--maxfailuresdisplayed",
-                    str(args.maxfailuresdisplayed),
-                    "--format",
-                    args.format,
-                ]
-
-                command.append(args.input)
-
-                stdout, stderr, return_code = run_arlington(" ".join(command))
-
-                if output_file:
-                    with open(args.output, "w+", encoding="utf-8") as out:
-                        out.write(stdout)
-
-                else:
-                    print(stdout)
-
-                if stderr:
-                    print(stderr, file=sys.stderr)
-
-            except Exception as e:
-                sys.exit("Failed to run validation: {}".format(e))
-
-        else:
-            print("Input file must be PDF")
+    # Run subcommand
+    try:
+        args.func(args)
+    except Exception as e:
+        print(traceback.format_exc(), file=sys.stderr)
+        print(f"Failed to run the program: {e}", file=sys.stderr)
+        sys.exit(1)
 
 
 if __name__ == "__main__":
